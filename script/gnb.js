455d64230492d589accd15a324d84c2f1fe88aed
xururuca9797 <xruruca-@naver.com>
2020-10-29 19:58:38 +0900

    [update] - 메인이미지 변경

--- script/gnb.js
+++ script/gnb.js
@@ -1,38 +1,42 @@
 //gnb bar jq
 let num = 0; //0닫힘, 1열림
 $("#ham").click(function () {
-    if (num == 1) {
-        //닫힘상태면 열어라
-        num = 0; //열림상태
-        $(".h_inner").animate({
-                left: "0",
-            },
-            500
-        );
-        $(".tab_menu").animate({
-                left: "-300%",
-            },
-            600
-        );
-        $("section img:first-child").animate({
-            left: "0%",
-        });
-    } else if (num == 0) {
-        //열림상태면 닫아라
+  if (num == 1) {
+    //닫힘상태면 열어라
+    num = 0; //열림상태
+    $(".h_inner").animate(
+      {
+        left: "0",
+      },
+      500
+    );
+    $(".tab_menu").animate(
+      {
+        left: "-300%",
+      },
+      600
+    );
+    $("section img:first-child").animate({
+      left: "0%",
+    });
+  } else if (num == 0) {
+    //열림상태면 닫아라
 
-        num = 1; //닫힘상태
-        $(".h_inner").animate({
-                left: "-300%",
-            },
-            500
-        );
-        $(".tab_menu").animate({
-                left: "0%",
-            },
-            600
-        );
-        $("section img:first-child").animate({
-            left: "-7%",
-        });
-    }
+    num = 1; //닫힘상태
+    $(".h_inner").animate(
+      {
+        left: "-300%",
+      },
+      500
+    );
+    $(".tab_menu").animate(
+      {
+        left: "0%",
+      },
+      600
+    );
+    $("section img:first-child").animate({
+      left: "-7%",
+    });
+  }
 }); ///click///
